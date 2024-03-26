a1e7b19444a71db63634a6f86fa19d63a2743a4c
Shivanshu-Baranwal <155421957+Shivanshu-Baranwal@users.noreply.github.com>
2024-03-26 18:28:47 +0530

    Handlebars

--- src/main/java/com/brightly/service/EmailResouce.java
+++ src/main/java/com/brightly/service/EmailResouce.java
@@ -5,9 +5,9 @@ import com.brightly.mailengine.proto.Messages;
 import com.brightly.mailengine.proto.MutinyMailEngineGrpc;
 import com.brightly.mailengine.proto.Response;
 
-import com.github.mustachejava.DefaultMustacheFactory;
-import com.github.mustachejava.Mustache;
-import com.github.mustachejava.MustacheFactory;
+
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
 import io.quarkus.grpc.GrpcService;
 import io.quarkus.mailer.Mail;
 import io.smallrye.mutiny.Uni;
@@ -15,7 +15,8 @@ import jakarta.inject.Inject;
 import org.jboss.logging.Logger;
 
 import java.io.IOException;
-import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -31,43 +32,47 @@ public class EmailResouce extends MutinyMailEngineGrpc.MailEngineImplBase {
     @Override
     public Uni<Response> sendMessage(Messages request) {
 
-
-
         List<Message> msgs = request.getMessagesList();
         List<Mail> mails = new ArrayList<>();
 
-        String htmlContent = "";
-        try {
-
-            for(Message msg : msgs) {
-                // Render the template with dynamic values
-                StringWriter stringWriter = new StringWriter();
-
-                // Load the template
-                MustacheFactory mf = new DefaultMustacheFactory();
-                Mustache mustache = mf.compile("templates/mustache/email_template.mustache");
-
-                Map<String, Object> data = new HashMap<>();
-                data.put("subject", msg.getSubject());
-                data.put("body", msg.getBody());
-
-                mustache.execute(stringWriter, data).flush();
-                htmlContent = stringWriter.toString();
-
-                mails.add(Mail.withHtml(msg.getEmail(), msg.getSubject(), htmlContent));
-            }
-        }
-        catch (IOException e) {
-            throw new RuntimeException(e.getMessage());
+        for (Message msg : msgs) {
+            String htmlContent = renderEmail(msg.getSubject(), msg.getBody());
+            mails.add(Mail.withHtml(msg.getEmail(), msg.getSubject(), htmlContent));
         }
 
-        for(Mail mail : mails) {
-            logger.info("\n" + mail.getTo() + "\n" + mail.getSubject() + "\n" + mail.getHtml()+ "\n\n");
+        for (Mail mail : mails) {
+        logger.info("\n" + mail.getTo() + "\n" + mail.getSubject() + "\n" + mail.getHtml());
         }
 
         return Uni.createFrom().item(Response.newBuilder().setResponseMessage("Successfully Send Email").build());
     }
 
+    public String renderEmail(String sub, String body) {
+        String renderedTemplate = "";
+        try {
 
+            // Load the Handlebars template from file
+            String templatePath = "C:\\Users\\z004vxjk\\IdeaProjects\\mail-engine\\src\\main\\resources\\templates\\handlebars\\email-template.hbs";
+            String templateContent = new String(Files.readAllBytes(Paths.get(templatePath)));
+
+            // Initialize Handlebars engine
+            Handlebars handlebars = new Handlebars();
+
+            // Compile the template
+            Template template = handlebars.compileInline(templateContent);
+
+            // Prepare data to render
+            Map<String, Object> data = new HashMap<>();
+            data.put("subject", sub);
+            data.put("body", body);
+
+            // Render the template with data
+            renderedTemplate = template.apply(data);
+
+        }catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        return renderedTemplate;
+    }
 }
 
